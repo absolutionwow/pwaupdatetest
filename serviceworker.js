db5a951ec8c84b8f1ebed7d5ab745207b4091dc8
absolutionwow <andreas.dahlp@gmail.com>
2022-10-04 10:30:29 +0200

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,17 @@
+const cacheName = "cache-pwajson";
+
+//Når websitet indlæses, cache ressourcer i listen //
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            return cache.addAll(["/pwaupdatetest/", "/pwaupdatetest/index.html", "pwaupdatetest/morten.pmg", "pwaupdatetest/nina.png", "pwaupdatetest/olivia.png"])
+        })
+    )
+})
+
+self.addEventListener("fetch", function(event){
+    event.respondWIth(fetch(event.request).catch(()=>
+    caches.open(cacheName).then(cache=>cache.match(event.request))
+    ))
+})
