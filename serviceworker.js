0ed9e98dfaba452a0fde2d21ee463bc6fbc4744d
absolutionwow <andreas.dahlp@gmail.com>
2022-10-04 10:36:31 +0200

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -5,7 +5,7 @@ const cacheName = "cache-pwajson";
 self.addEventListener("install", function (event) {
     event.waitUntil(
         caches.open(cacheName).then(function(cache){
-            return cache.addAll(["/pwaupdatetest/", "/pwaupdatetest/index.html", "pwaupdatetest/morten.pmg", "pwaupdatetest/nina.png", "pwaupdatetest/olivia.png"])
+            return cache.addAll(["/pwaupdatetest/", "/pwaupdatetest/index.html", "pwaupdatetest/morten.pmg", "pwaupdatetest/nina.png", "pwaupdatetest/olivia.png", "pwaupdatetest/javascript.js"])
         })
     )
 })
@@ -15,3 +15,5 @@ self.addEventListener("fetch", function(event){
     caches.open(cacheName).then(cache=>cache.match(event.request))
     ))
 })
+
+console.log("update 1")
